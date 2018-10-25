9c1cb29a2f323a634ed9cd4770d46223267be87e
jroaacun4621 <jroaacun4621@universidadean.edu.co>
2018-10-25 00:56:40 -0500

    Initial config

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Callback from '@/components/Callback'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/callback',
+      name: 'Callback',
+      component: Callback
+    },
+    {
+      path: '*',
+      redirect: '/home'
+    }
+  ]
+})
