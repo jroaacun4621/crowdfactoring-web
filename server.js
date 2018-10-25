bca0677d12c4778c9430b67d38ac01de906a7b69
jroaacun4621 <jroaacun4621@universidadean.edu.co>
2018-10-25 01:22:56 -0500

    Agregado server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// route all urls to vue js router
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html')
+})
+
+app.set('port', process.env.PORT || 8080);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('listening on port ', server.address().port);
+});
